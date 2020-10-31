e2d2394ea3932319235087d01b569f030e237d91
k00lawn <k00lawn@gmail.com>
2020-10-31 18:50:32 +0530

    structure change

--- backend/nodeserver/index.js
+++ backend/nodeserver/index.js
@@ -1,27 +1,30 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require("path");
-const mongoose = require('mongoose')
-const PORT = process.env.PORT || 3000
+const path = require('path');
+const dotenv = require('dotenv')
+const connectDB = require('./config/db')
 
 const postsRoutes = require('../nodeserver/routes/posts') 
 
 //atlas pw igxQtkHMS1DGKddF const uri = "mongodb+srv://<username>:<password>@cluster0.mzdpt.mongodb.net/<dbname>?retryWrites=true&w=majority";
 
+dotenv.config({ path: './config/config.env' })
+
+connectDB()
 
 const app = express();
 
-mongoose
-  .connect(
-    "mongodb://k00l:igxQtkHMS1DGKddF@cluster0-shard-00-00.mzdpt.mongodb.net:27017,cluster0-shard-00-01.mzdpt.mongodb.net:27017,cluster0-shard-00-02.mzdpt.mongodb.net:27017/opmdb?ssl=true&replicaSet=atlas-dvcex0-shard-0&authSource=admin&retryWrites=true&w=majority",
-    { useNewUrlParser: true }
-  )
-  .then(() => {
-    console.log("Connected to database!");
-  })
-  .catch(error => {
-    console.log("Connection failed!", error);
-  });
+// mongoose
+//   .connect(
+//     "mongodb://k00l:igxQtkHMS1DGKddF@cluster0-shard-00-00.mzdpt.mongodb.net:27017,cluster0-shard-00-01.mzdpt.mongodb.net:27017,cluster0-shard-00-02.mzdpt.mongodb.net:27017/opmdb?ssl=true&replicaSet=atlas-dvcex0-shard-0&authSource=admin&retryWrites=true&w=majority",
+//     { useNewUrlParser: true }
+//   )
+//   .then(() => {
+//     console.log("Connected to database!");
+//   })
+//   .catch(error => {
+//     console.log("Connection failed!", error);
+//   });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -40,7 +43,14 @@ app.use((req, res, next) => {
     next();
 });
 
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
 app.use("/api/postTask", postsRoutes)
+
+const PORT = process.env.PORT || 3000
+
 
 app.listen(PORT, () => {
   console.log(`Server up on ${PORT}` )
