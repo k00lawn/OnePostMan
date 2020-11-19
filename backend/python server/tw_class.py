240ea3eb413f48b32ff33fba29a80ca7e04d78da
jaabir <jaabir.zaid@gmail.com>
2020-11-19 15:33:19 +0530

    changed the tw class file

--- backend/python server/tw_class.py
+++ backend/python server/tw_class.py
@@ -29,64 +29,96 @@ class TwitterApi():
 
     def make_auth(self):
 
-        tw_details = self.creds()
-        auth = tweepy.OAuthHandler(tw_details['app_key'],tw_details['app_secret'])
-        auth.set_access_token(self.token,self.token_secret)
-        api = tweepy.API(auth,wait_on_rate_limit=True,wait_on_rate_limit_notify=True)
+        try:
+            tw_details = self.creds()
+            auth = tweepy.OAuthHandler(tw_details['app_key'],tw_details['app_secret'])
+            auth.set_access_token(self.token,self.token_secret)
+            api = tweepy.API(auth,wait_on_rate_limit=True,wait_on_rate_limit_notify=True)
 
-        return api
+            return api
+        
+        except tweepy.error.TweepError as error:
+            return {'error': error}
 
     def user_details(self):
 
         api = self.make_auth()
-        me = api.me()
-        details = dict()
+        
+        try:
+            me = api.me()
+            details = dict()
 
-        details['user_id'] = me.id
-        details['name'] = me.name
-        details['followers'] = me.followers_count
-        details['followings'] = me.friends_count
-        details['screen_name'] = me.screen_name
-        details['user_time_zone'] = me.time_zone
+            details['user_id'] = me.id
+            details['name'] = me.name
+            details['followers'] = me.followers_count
+            details['followings'] = me.friends_count
+            details['screen_name'] = me.screen_name
+            details['user_time_zone'] = me.time_zone
 
-        return details
+            return details
+        
+        except tweepy.error.TweepError as error:
+            return {'error': error}
+        
+        except KeyError as error:
+            return {'error': error}
 
     def get_tweets(self):
 
         api = self.make_auth()
 
-        tweets_data = dict()
-        tweets_data['tweets'] = []
-        tweets_data['id'] = []
-        tweets_data['created_on'] = []
-        tweets_data['retweet_count'] = []
-        tweets_data['favorite_count'] = []
-        tweets_data['language'] = []
+        try:
+            tweets_data = dict()
+            tweets_data['tweets'] = []
+            tweets_data['id'] = []
+            tweets_data['created_on'] = []
+            tweets_data['retweet_count'] = []
+            tweets_data['favorite_count'] = []
+            tweets_data['language'] = []
 
-        tweets = api.user_timeline()
+            tweets = api.user_timeline()
 
-        for i in tweets:
-            tweets_data['tweets'].append(i.text)
-            tweets_data['id'].append(i.id)
-            tweets_data['created_on'].append(i.created_at)
-            tweets_data['retweet_count'].append(i.retweet_count)
-            tweets_data['favorite_count'].append(i.favourite_count)
-            tweets_data['language'].append(i.lang)
+            for i in tweets:
+                tweets_data['tweets'].append(i.text)
+                tweets_data['id'].append(i.id)
+                tweets_data['created_on'].append(i.created_at)
+                tweets_data['retweet_count'].append(i.retweet_count)
+                tweets_data['favorite_count'].append(i.favourite_count)
+                tweets_data['language'].append(i.lang)
 
-        return tweets_data
+            return tweets_data
+        
+        except tweepy.error.TweepError as error:
+            return {'error': error}
+        
+        except KeyError as error:
+            return {'error': error}
+
 
     def get_replies(self):
         api = self.make_auth()
-        mentions = []
-        for i in tweepy.Cursor(api.mentions_timeline,count=200).items(1000):
-            rid = i.in_reply_to_status_id
-            replied_user_sname = i.user.screen_name
-            replied_user_name = i.user.name
-            reply = i.text
-            tweet = api.get_status(rid).text
-            mentions.append((replied_user_name,replied_user_sname,reply,tweet))
+        
+        try:
+            mentions = []
+            for i in tweepy.Cursor(api.mentions_timeline,count=200).items(1000):
+                rid = i.in_reply_to_status_id
+                replied_user_sname = i.user.screen_name
+                replied_user_name = i.user.name
+                reply = i.text
+                tweet = api.get_status(rid).text
+                mentions.append((replied_user_name,replied_user_sname,reply,tweet))
 
-        return mentions
+            return mentions
+        
+        except tweepy.error.TweepError as error:
+            return {'error': error}
+        
+        except ValueError as error:
+            return {'error': error}
+        
+        except KeyError as error:
+            return {'error': error}
+
 
     def post_tweet(self):
 
@@ -106,9 +138,13 @@ class TwitterApi():
 
         api = self.make_auth()
 
-        if self.img is not None:
-            print('posting with the img')
-            api.update_with_media(self.img,self.msg)
-        else:
-            print('posting without img')
-            api.update_status(self.msg)
+        try:
+            if self.img is not None:
+                print('posting with the img')
+                api.update_with_media(self.img,self.msg)
+            else:
+                print('posting without img')
+                api.update_status(self.msg)
+
+        except tweepy.error.TweepError as error:
+            return {'error': error}
