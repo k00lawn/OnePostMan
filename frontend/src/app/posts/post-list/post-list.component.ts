cfcd8ed70852f48192f2688b3b99d4bb6a58b36f
jaabir <jaabir.zaid@gmail.com>
2021-02-04 13:59:12 +0530

    see the app now

--- frontend/src/app/posts/post-list/post-list.component.ts
+++ frontend/src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit} from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Post } from 'src/app/models/post';
 import { User } from 'src/app/models/user';
