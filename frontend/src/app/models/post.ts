9dc1d288c42cc99438913835fa166be795841111
k00lawn <k00lawn@gmail.com>
2021-02-12 22:27:36 +0530

    angular build

--- frontend/src/app/models/post.ts
+++
@@ -1,9 +0,0 @@
-export interface Post {
-    _id: string,
-    userId: string,
-    caption: string,
-    date: string,
-    img: string,
-    facebook: string,
-    twitter: string
-} 
--- frontend/src/app/models/user.ts
+++
@@ -1,6 +0,0 @@
-export interface User {
-    user_id: String,
-    username: String,
-    fb_provider: Boolean,
-    tw_provider: Boolean
-}
