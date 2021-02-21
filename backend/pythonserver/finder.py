87043646152180e040c0f720821a09acfe4016be
jaabir <jaabir.zaid@gmail.com>
2021-02-21 10:19:05 +0530

    gmmmm now it works

--- backend/pythonserver/finder.py
+++ backend/pythonserver/finder.py
@@ -1,4 +1,7 @@
 import os 
 
-print(os.getcwd())
 
+print(f"Current working dir : {os.getcwd()}")
+dir = os.listdir()
+print(f"is there nodeserver in this dir : {'nodeserver' in dir}")
+print(f"listing all the dir inside this dir : {dir}")
